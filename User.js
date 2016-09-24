2d0a495a3871126c39883910765c0bb6a9806e37
adrian hartanto <adrianasdfs@gmail.com>
2016-09-24 16:37:20 +0700

    assign random id to every user

--- User.js
+++ User.js
@@ -1,6 +1,11 @@
+
+var Chance = require('chance');
+
 function User(){
+
+	var chance = new Chance();
 	// generate random id
-	this._userId = "asdasdas"
+	this._userId = chance.string({length:5});
 }
 
 User.prototype.getUserId = function getUserId(){
