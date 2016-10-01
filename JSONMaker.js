f4d167e271c78e9176268ab6ebee42e8f387ce2a
adrian hartanto <adrianasdfs@gmail.com>
2016-10-01 23:09:05 +0700

    make roomlist json response

--- JSONMaker.js
+++ JSONMaker.js
@@ -1,18 +1,34 @@
 function JSONMaker(){}
 
-JSONMaker.makeGreetJSON = function(roomDetails,userID){
+function createJson(code, data){
 
-	var output = [{
-			"response":{
-				"type":"greet",
-				"data":{
-					"round":roomDetails.round.toString(),
-					"user":userID
-					}
-				}
-			}];
+     var output = [{
+            "response":{
+                "code":code,
+                "data":data
+            }
+    }]    
 
-	return output;
+     return output;
 }
+
+
+JSONMaker.makeGreetJSON = function(room,userID,code){
+
+    var details = room.getRoomDetails();
+    details["user"] = userID;
+            
+	return createJson(code,details);
+}
+
+
+JSONMaker.makeRoomListJSON = function(roomList,code){
+    
+    var ar = [];
+    roomList.forEach(function(item,index){
+        ar.push(item.getRoomDetails());
+    }) 
+
+    return createJson(code,ar);
 
 module.exports = JSONMaker
