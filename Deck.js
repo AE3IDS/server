5225bec47d74f65b85302ae0f0f3103072f1da2d
adrian hartanto <adrianasdfs@gmail.com>
2016-10-10 22:11:13 +0700

    object representing a deck of cards

---
+++ Deck.js
@@ -0,0 +1,21 @@
+var Card = require('./Card');
+
+function Deck(){
+    
+    this._cards = [];
+
+    for(var i = 1; i < 5;i++){
+    
+        var card = [];
+        for(var j = 3; j < 16;j++){
+            card.push(new Card(i,j));
+        }
+        this._cards.push(card);
+
+    }
+
+}
+
+Deck.prototype.shuffle = function shuffle(){
+
+}
