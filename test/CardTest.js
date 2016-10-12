563c1c32d99911be9a27957a1352546b476a7298
adrian hartanto <adrianasdfs@gmail.com>
2016-10-12 14:57:16 +0700

    add isCardStronger test

--- test/CardTest.js
+++ test/CardTest.js
@@ -12,6 +12,13 @@ describe("CardTest",function(){
         expect(c.isCardEqual(m)).to.equal(true);
     });
 
+    it("isCardStronger shall return true if compared a card that has lower strength",function(){
 
+        var c = new Card(0,5);
+        var g = new Card(2,10);
+
+        expect(g.isCardStronger(c)).to.equal(true);
+
+    });
 
 })
