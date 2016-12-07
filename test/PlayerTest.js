7749204f96e206e7ff516512f637a0e23abdbf17
adrian hartanto <adrianasdfs@gmail.com>
2016-12-07 22:46:09 +0700

    add test for addDealtCards

--- test/PlayerTest.js
+++ test/PlayerTest.js
@@ -38,5 +38,43 @@ describe("PlayerTest",function(){
 
     });
 
+    it("addDealtCards should add new object of type Cards to dealthCards",function(){
+
+        var player = new Player(1,undefined);
+        var data = [];
+
+        var randomCounter = Math.floor(Math.random() * (10 - 0 + 1)) + 0;
+
+        for(var i = 0;i < randomCounter;i++){
+            
+            var randomSuit =  Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+            var randomKind = Math.floor(Math.random() * (15 - 3 +1)) + 3;
+            var content = {"suit":randomSuit,"kind":randomKind};
+            data.push(content);
+
+        }
+
+        player.addDealtCards(data);
+        
+        var dealtCards = player.getDealtCards();
+
+        /* Check same length */
+
+        var sameLength = dealtCards.length == data.length;
+
+        expect(sameLength).to.equal(true);
+
+        /* Check content */
+
+        var hasAllData = true;
+
+        dealtCards.forEach(function(item,index){
+            var sameSuit = item.getSuit() == data[index]["suit"];
+            var sameKind = item.getKind() == data[index]["kind"];
+            expect(sameSuit && sameKind).to.equal(true);
+        });
+
+    });    
+
 })
 
