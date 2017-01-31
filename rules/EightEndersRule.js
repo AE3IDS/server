c27e75520a2d1d9cb5343f615fd816d34da03326
adrian hartanto <adrianasdfs@gmail.com>
2017-01-31 23:01:31 +0700

    change module definition

--- rules/EightEndersRule.js
+++ rules/EightEndersRule.js
@@ -3,14 +3,15 @@ var Rule = require("./Rule");
 const EIGHT_NUMBER = 8;
 
 function EightEndersRule(){
-    Rule.call(this,false,1);
+    
+    var name = "Eight Enders";
+    var description = "Terminate the round when any player deals and 8 card";
+    var ruleId = "R1";
+
+    Rule.call(this,name,description,ruleId);
 }
 
 EightEndersRule.prototype = new Rule();
-
-EightEndersRule.prototype.getId = function getId(){
-    return this.ruleId;
-}
 
 EightEndersRule.prototype.checkCard = function checkCard(cards){
     return (cards.indexOf(EIGHT_NUMBER) == -1);
