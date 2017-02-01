967ea86e832e0543cfd7f4f9fc23ba1ef3547bb4
adrian hartanto <adrianasdfs@gmail.com>
2017-02-01 12:47:07 +0700

    new module for the Revolution rule

--- rules/Revolution.js
+++ rules/Revolution.js
@@ -1,21 +1,15 @@
 var Rule = require("./Rule");
 
-const EIGHT_NUMBER = 8;
-
-function EightEndersRule(){
+function Revolution(){
     
-    var name = "Eight Enders";
-    var description = "Terminate the round when any player deals and 8 card";
-    var ruleId = "R1";
+    var name = "Revolution";
+    var description = "asdfs";
+    var ruleId = "R4";
 
     Rule.call(this,name,description,ruleId);
 }
 
-EightEndersRule.prototype = new Rule();
+Revolution.prototype = new Rule();
 
-EightEndersRule.prototype.checkCard = function checkCard(cards){
-    return (cards.indexOf(EIGHT_NUMBER) == -1);
-}
-
-module.exports = EightEndersRule;
+module.exports = Revolution;
 
