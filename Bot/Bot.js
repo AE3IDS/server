1fd8c4364885712fb91610b59bdeaaf3ecf69c0d
adrian hartanto <adrianasdfs@gmail.com>
2017-02-11 15:26:31 +0700

    add _photoId, add setters and getters

--- Bot/Bot.js
+++ Bot/Bot.js
@@ -3,6 +3,28 @@ var Chance = require('chance').Chance();
 function Bot(){
 
     this._userId = Chance.string({length:5}); 
+    this._photoId = -1;
+}
+
+
+Bot.prototype.setPhotoId = function setPhotoId(selectedPhotoIds)
+{
+    while(true)
+    {
+        var id = Chance.natural({min:0,max:8});
+        
+        if(selectedPhotoIds.indexOf(id) == -1)
+        {    
+            this._photoId = id;
+            break;
+        }
+    }            
+}
+
+
+Bot.prototype.getPhotoId = function getPhotoId()
+{
+    return this._photoId;
 }
 
 module.exports = Bot;
