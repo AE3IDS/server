fa19883dea287a37ecde6a13fca3387110b3dced
adrian hartanto <adrianasdfs@gmail.com>
2017-02-11 22:58:21 +0700

    use timer to spawn the bot

--- BotSpawner.js
+++ BotSpawner.js
@@ -1,16 +1,28 @@
 const spawn = require('child_process').spawn
 const fs = require('fs');
-const out = fs.openSync('./out.log', 'a');
+const out = fs.openSync('./Bot/stdout.log', 'a');
 
 
 function BotSpawner(roomId, numOfBot)
 {
     this._roomId = roomId;
-    for(var i = 0;i < numOfBot;i++)
-    {
+
+
+   var spawnFunction = function(){
+
         const ls = spawn("/usr/local/bin/node",["Bot/ClientMain.js", roomId],{detached:true, stdio:['ignore',out, out]})
         ls.unref();
+
+        numOfBot--;
+
+        if(numOfBot != 0)
+            setTimeout(spawnFunction,1500);
+
     }
+
+    spawnFunction();
+
+
 }
 
 BotSpawner.spawn = function spawn(numOfBot)
