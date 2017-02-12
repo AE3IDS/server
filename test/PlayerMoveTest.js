03b167095324374747cf8dd24aada0c765ddce6b
adrian hartanto <adrianasdfs@gmail.com>
2017-02-13 00:48:02 +0700

    merge PlayerMoveTest from game branch

--- test/PlayerMoveTest.js
+++ test/PlayerMoveTest.js
@@ -1,12 +1,17 @@
 var chai = require('chai');
 var expect = chai.expect;
+var Eight = require('../rules/EightEndersRule');
+var Jack = require('../rules/JackBack');
+var Jokers = require('../rules/JokersAreWild');
+var Revolution = require('../rules/Revolution');
 var PlayerMove = require('../PlayerMove');
+var GeneralRule = require('../rules/GeneralRule');
 var Card = require('../Card');
 var chance = require('chance').Chance();
 
 describe("PlayerMoveTest",function(){
     
-    it("isMoveStronger shall return true if given a set of cards that have weakers trength, as argument",function(){
+    it("isMoveStronger shall return true if given a set of cards that have weakers trength, as argument reverse is false",function(){
         
         var weakCardKind = chance.integer({min:3,max:6});
         var strongCardKind = chance.integer({min:7,max:14});
@@ -29,40 +34,313 @@ describe("PlayerMoveTest",function(){
         var weakCardsMove = new PlayerMove(false,"asdfs",weakCards);
         var strongCardsMove = new PlayerMove(false,"asdfs",strongCards);
 
-        var isStronger = strongCardsMove.isMoveStronger(weakCardsMove);
+        var isStronger = strongCardsMove.isMoveStronger(false, weakCardsMove);
+
+        expect(isStronger).to.equal(true);
+
+    });
+
+    it("isMoveStronger shall return false if given a set of cards that have weakers trength, as argument reverse is true",function(){
+        
+        var weakCardKind = chance.integer({min:3,max:6});
+        var strongCardKind = chance.integer({min:7,max:14});
+
+        var randWeakCardSuit = chance.integer({min:1,max:4});
+        var randStrongCardSuit = chance.integer({min:1,max:4});
+
+        var weakCards = [];
+        var strongCards = [];
+
+        for(var i = 0; i < 3; i++)
+        {
+            var weakCard = new Card(randWeakCardSuit, weakCardKind);
+            var strongCard = new Card(randStrongCardSuit, strongCardKind);
+
+            weakCards.push(weakCard);
+            strongCards.push(strongCard);
+        }
+
+        var weakCardsMove = new PlayerMove(false,"asdfs",weakCards);
+        var strongCardsMove = new PlayerMove(false,"asdfs",strongCards);
+
+        var isStronger = strongCardsMove.isMoveStronger(true, weakCardsMove);
+
+        expect(isStronger).to.equal(false);
+
+    });
+
+
+
+
+
+    it("isMoveStronger shall return false if given a set of cards that have higher strength, as argument and reverse is false",function(){
+        
+        var weakCardKind = chance.integer({min:3,max:6});
+        var strongCardKind = chance.integer({min:7,max:14});
+
+        var randWeakCardSuit = chance.integer({min:1,max:4});
+        var randStrongCardSuit = chance.integer({min:1,max:4});
+
+        var weakCards = [];
+        var strongCards = [];
+
+        for(var i = 0; i < 3; i++)
+        {
+            var weakCard = new Card(randWeakCardSuit, weakCardKind);
+            var strongCard = new Card(randStrongCardSuit, strongCardKind);
+
+            weakCards.push(weakCard);
+            strongCards.push(strongCard);
+        }
+
+        var strongCardsMove = new PlayerMove(false,"asdfs",strongCards);
+        var weakCardsMove = new PlayerMove(false,"asdfs",weakCards);
+
+        var isStronger = weakCardsMove.isMoveStronger(false, strongCardsMove);
+
+        expect(isStronger).to.equal(false);
+
+    });
+
+    it("isMoveStronger shall return true if given a set of cards that have higher strength, as argument and reverse is false",function(){
+        
+        var weakCardKind = chance.integer({min:3,max:6});
+        var strongCardKind = chance.integer({min:7,max:14});
+
+        var randWeakCardSuit = chance.integer({min:1,max:4});
+        var randStrongCardSuit = chance.integer({min:1,max:4});
+
+        var weakCards = [];
+        var strongCards = [];
+
+        for(var i = 0; i < 3; i++)
+        {
+            var weakCard = new Card(randWeakCardSuit, weakCardKind);
+            var strongCard = new Card(randStrongCardSuit, strongCardKind);
+
+            weakCards.push(weakCard);
+            strongCards.push(strongCard);
+        }
+
+        var strongCardsMove = new PlayerMove(false,"asdfs",strongCards);
+        var weakCardsMove = new PlayerMove(false,"asdfs",weakCards);
+
+        var isStronger = weakCardsMove.isMoveStronger(true, strongCardsMove);
 
         expect(isStronger).to.equal(true);
 
     });
 
 
-    it("isMoveStronger shall return false if given a set of cards that have higher strength, as argument",function(){
+    /* checkExtraRules test */
+
+    it("checkExtraRules shall return undefined if there are no rules that are applicable for an array of cards that doesnt satisfy any rules",function(){
+
+        /* initialize rules */
+
+        var general = new GeneralRule();
+        var jackRule = new Jack();
+        var revolution = new Revolution();
+        var eightRule = new Eight();
+        var jokerRule = new Jokers();
+
+        var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+         // Input: array of 3 Card items with card rank/kind 3 
         
-        var weakCardKind = chance.integer({min:3,max:6});
-        var strongCardKind = chance.integer({min:7,max:14});
-
-        var randWeakCardSuit = chance.integer({min:1,max:4});
-        var randStrongCardSuit = chance.integer({min:1,max:4});
-
-        var weakCards = [];
-        var strongCards = [];
+        var cards = [];
 
         for(var i = 0; i < 3; i++)
         {
-            var weakCard = new Card(randWeakCardSuit, weakCardKind);
-            var strongCard = new Card(randStrongCardSuit, strongCardKind);
-
-            weakCards.push(weakCard);
-            strongCards.push(strongCard);
+            var randSuit = chance.natural({min:1,max:4});
+            var newCard = new Card(randSuit, 3);
+            cards.push(newCard);
         }
 
-        var strongCardsMove = new PlayerMove(false,"asdfs",strongCards);
-        var weakCardsMove = new PlayerMove(false,"asdfs",weakCards);
+        var plMove = new PlayerMove(undefined, undefined, cards);
 
-        var isStronger = weakCardsMove.isMoveStronger(strongCardsMove);
+        var output = plMove.checkExtraRules(rules);
 
-        expect(isStronger).to.equal(false);
+        expect(output == undefined).to.equal(true);
+    });
 
+    it("checkExtraRules shall return an object with the 'now' property that only has 'Jack back', if applicabale rules are jack back without any Eight Enders"
+        ,function(){
+
+        /* initialize rules */
+
+        var general = new GeneralRule();
+        var jackRule = new Jack();
+        var revolution = new Revolution();
+        var eightRule = new Eight();
+        var jokerRule = new Jokers();
+
+        var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+        /* Input: array of 3 Card items with card rank/kind 11 */
+        
+        var cards = [];
+
+        for(var i = 0; i < 3; i++)
+        {
+            var randSuit = chance.natural({min:1,max:4});
+            var newCard = new Card(randSuit,11);
+            cards.push(newCard);
+        }
+
+        var plMove = new PlayerMove(undefined, undefined, cards);
+
+        var output = plMove.checkExtraRules(rules);
+
+        var nowProperty = output["now"];
+
+        expect(nowProperty != undefined).to.equal(true);
+        expect(nowProperty.length == 1).to.equal(true);
+        expect(nowProperty.pop() == "Jack Back").to.equal(true);
+    });
+
+    it("checkExtraRules shall return an object with the newRound property that has false value, if applicabale rules are jack back without any Eight Enders"
+        ,function(){
+
+        /* initialize rules */
+
+        var general = new GeneralRule();
+        var jackRule = new Jack();
+        var revolution = new Revolution();
+        var eightRule = new Eight();
+        var jokerRule = new Jokers();
+
+        var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+        /* Input: array of 3 Card items with card rank/kind 11 */
+        
+        var cards = [];
+
+        for(var i = 0; i < 3; i++)
+        {
+            var randSuit = chance.natural({min:1,max:4});
+            var newCard = new Card(randSuit,11);
+            cards.push(newCard);
+        }
+
+        var plMove = new PlayerMove(undefined, undefined, cards);
+
+        var output = plMove.checkExtraRules(rules);
+
+        var nowProperty = output["newRound"];
+
+        expect(nowProperty != undefined).to.equal(true);
+        expect(nowProperty == false).to.equal(true);
+    });
+
+    it("checkExtraRules shall return an object with the newRound property that has true value, if applicabale rules are Eight Enders followed by Revolution"
+        ,function(){
+
+        /* initialize rules */
+
+        var general = new GeneralRule();
+        var jackRule = new Jack();
+        var revolution = new Revolution();
+        var eightRule = new Eight();
+        var jokerRule = new Jokers();
+
+        var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+        /* Input: array of 4 Card items with card rank/kind 8 */
+        
+        var cards = [];
+
+        for(var i = 0; i < 4; i++)
+        {
+            var randSuit = chance.natural({min:1,max:4});
+            var newCard = new Card(randSuit,8);
+            cards.push(newCard);
+        }
+
+        var plMove = new PlayerMove(undefined, undefined, cards);
+
+        var output = plMove.checkExtraRules(rules);
+
+        var nowProperty = output["newRound"];
+
+        expect(nowProperty != undefined).to.equal(true);
+        expect(nowProperty == true).to.equal(true);
+    });
+
+    it("checkExtraRules shall return an object with the 'now' property that only has 'Eight Enders', if applicabale rules are Eight Enders followed by Revolution"
+        ,function(){
+
+        /* initialize rules */
+
+        var general = new GeneralRule();
+        var jackRule = new Jack();
+        var revolution = new Revolution();
+        var eightRule = new Eight();
+        var jokerRule = new Jokers();
+
+        var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+         // Input: array of 4 Card items with card rank/kind 8 
+        
+        var cards = [];
+
+        for(var i = 0; i < 4; i++)
+        {
+            var randSuit = chance.natural({min:1,max:4});
+            var newCard = new Card(randSuit,8);
+            cards.push(newCard);
+        }
+
+        var plMove = new PlayerMove(undefined, undefined, cards);
+
+        var output = plMove.checkExtraRules(rules);
+
+        var nowProperty = output["now"];
+
+        expect(nowProperty != undefined).to.equal(true);
+        expect(nowProperty.length == 1).to.equal(true);
+        expect(nowProperty.pop() == "Eight Enders").to.equal(true);
+    });
+
+    it("checkExtraRules shall return an object with the 'later' property that only has 'Revolution', if applicabale rules are Eight Enders followed by Revolution"
+        ,function(){
+
+        /* initialize rules */
+
+        var general = new GeneralRule();
+        var jackRule = new Jack();
+        var revolution = new Revolution();
+        var eightRule = new Eight();
+        var jokerRule = new Jokers();
+
+        var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+         // Input: array of 4 Card items with card rank/kind 8 
+        
+        var cards = [];
+
+        for(var i = 0; i < 4; i++)
+        {
+            var randSuit = chance.natural({min:1,max:4});
+            var newCard = new Card(randSuit,8);
+            cards.push(newCard);
+        }
+
+        var plMove = new PlayerMove(undefined, undefined, cards);
+
+        var output = plMove.checkExtraRules(rules);
+
+        var nowProperty = output["later"];
+
+        expect(nowProperty != undefined).to.equal(true);
+        expect(nowProperty.length == 1).to.equal(true);
+        expect(nowProperty.pop() == "Revolution").to.equal(true);
     });
 
 
