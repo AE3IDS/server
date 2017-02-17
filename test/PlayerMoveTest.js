21c119338cce76e2854c56105a8e8b90e0f165c3
adrian hartanto <adrianasdfs@gmail.com>
2017-02-17 15:45:10 +0700

    add test for isMoveEqualTo

--- test/PlayerMoveTest.js
+++ test/PlayerMoveTest.js
@@ -134,214 +134,279 @@ describe("PlayerMoveTest",function(){
 
     /* checkExtraRules test */
 
-    it("checkExtraRules shall return undefined if there are no rules that are applicable for an array of cards that doesnt satisfy any rules",function(){
+    // it("checkExtraRules shall return undefined if there are no rules that are applicable for an array of cards that doesnt satisfy any rules",function(){
 
-        /* initialize rules */
+    //     /* initialize rules */
 
-        var general = new GeneralRule();
-        var jackRule = new Jack();
-        var revolution = new Revolution();
-        var eightRule = new Eight();
-        var jokerRule = new Jokers();
+    //     var general = new GeneralRule();
+    //     var jackRule = new Jack();
+    //     var revolution = new Revolution();
+    //     var eightRule = new Eight();
+    //     var jokerRule = new Jokers();
 
-        var rules = [general, jackRule, revolution, eightRule, jokerRule];
+    //     var rules = [general, jackRule, revolution, eightRule, jokerRule];
 
 
-         // Input: array of 3 Card items with card rank/kind 3 
+    //      // Input: array of 3 Card items with card rank/kind 3 
+        
+    //     var cards = [];
+
+    //     for(var i = 0; i < 3; i++)
+    //     {
+    //         var randSuit = chance.natural({min:1,max:4});
+    //         var newCard = new Card(randSuit, 3);
+    //         cards.push(newCard);
+    //     }
+
+    //     var plMove = new PlayerMove(undefined, undefined, cards);
+
+    //     var output = plMove.checkExtraRules(rules);
+
+    //     expect(output == undefined).to.equal(true);
+    // });
+
+    // it("checkExtraRules shall return an object with the 'now' property that only has 'Jack back', if applicabale rules are jack back without any Eight Enders"
+    //     ,function(){
+
+    //     /* initialize rules */
+
+    //     var general = new GeneralRule();
+    //     var jackRule = new Jack();
+    //     var revolution = new Revolution();
+    //     var eightRule = new Eight();
+    //     var jokerRule = new Jokers();
+
+    //     var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+    //     /* Input: array of 3 Card items with card rank/kind 11 */
+        
+    //     var cards = [];
+
+    //     for(var i = 0; i < 3; i++)
+    //     {
+    //         var randSuit = chance.natural({min:1,max:4});
+    //         var newCard = new Card(randSuit,11);
+    //         cards.push(newCard);
+    //     }
+
+    //     var plMove = new PlayerMove(undefined, undefined, cards);
+
+    //     var output = plMove.checkExtraRules(rules);
+
+    //     var nowProperty = output["now"];
+
+    //     expect(nowProperty != undefined).to.equal(true);
+    //     expect(nowProperty.length == 1).to.equal(true);
+    //     expect(nowProperty.pop() == "Jack Back").to.equal(true);
+    // });
+
+    // it("checkExtraRules shall return an object with the newRound property that has false value, if applicabale rules are jack back without any Eight Enders"
+    //     ,function(){
+
+    //     /* initialize rules */
+
+    //     var general = new GeneralRule();
+    //     var jackRule = new Jack();
+    //     var revolution = new Revolution();
+    //     var eightRule = new Eight();
+    //     var jokerRule = new Jokers();
+
+    //     var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+    //     /* Input: array of 3 Card items with card rank/kind 11 */
+        
+    //     var cards = [];
+
+    //     for(var i = 0; i < 3; i++)
+    //     {
+    //         var randSuit = chance.natural({min:1,max:4});
+    //         var newCard = new Card(randSuit,11);
+    //         cards.push(newCard);
+    //     }
+
+    //     var plMove = new PlayerMove(undefined, undefined, cards);
+
+    //     var output = plMove.checkExtraRules(rules);
+
+    //     var nowProperty = output["newRound"];
+
+    //     expect(nowProperty != undefined).to.equal(true);
+    //     expect(nowProperty == false).to.equal(true);
+    // });
+
+    // it("checkExtraRules shall return an object with the newRound property that has true value, if applicabale rules are Eight Enders followed by Revolution"
+    //     ,function(){
+
+    //     /* initialize rules */
+
+    //     var general = new GeneralRule();
+    //     var jackRule = new Jack();
+    //     var revolution = new Revolution();
+    //     var eightRule = new Eight();
+    //     var jokerRule = new Jokers();
+
+    //     var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+    //     /* Input: array of 4 Card items with card rank/kind 8 */
+        
+    //     var cards = [];
+
+    //     for(var i = 0; i < 4; i++)
+    //     {
+    //         var randSuit = chance.natural({min:1,max:4});
+    //         var newCard = new Card(randSuit,8);
+    //         cards.push(newCard);
+    //     }
+
+    //     var plMove = new PlayerMove(undefined, undefined, cards);
+
+    //     var output = plMove.checkExtraRules(rules);
+
+    //     var nowProperty = output["newRound"];
+
+    //     expect(nowProperty != undefined).to.equal(true);
+    //     expect(nowProperty == true).to.equal(true);
+    // });
+
+    // it("checkExtraRules shall return an object with the 'now' property that only has 'Eight Enders', if applicabale rules are Eight Enders followed by Revolution"
+    //     ,function(){
+
+    //     /* initialize rules */
+
+    //     var general = new GeneralRule();
+    //     var jackRule = new Jack();
+    //     var revolution = new Revolution();
+    //     var eightRule = new Eight();
+    //     var jokerRule = new Jokers();
+
+    //     var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+    //      // Input: array of 4 Card items with card rank/kind 8 
+        
+    //     var cards = [];
+
+    //     for(var i = 0; i < 4; i++)
+    //     {
+    //         var randSuit = chance.natural({min:1,max:4});
+    //         var newCard = new Card(randSuit,8);
+    //         cards.push(newCard);
+    //     }
+
+    //     var plMove = new PlayerMove(undefined, undefined, cards);
+
+    //     var output = plMove.checkExtraRules(rules);
+
+    //     var nowProperty = output["now"];
+
+    //     expect(nowProperty != undefined).to.equal(true);
+    //     expect(nowProperty.length == 1).to.equal(true);
+    //     expect(nowProperty.pop() == "Eight Enders").to.equal(true);
+    // });
+
+    // it("checkExtraRules shall return an object with the 'later' property that only has 'Revolution', if applicabale rules are Eight Enders followed by Revolution"
+    //     ,function(){
+
+    //     /* initialize rules */
+
+    //     var general = new GeneralRule();
+    //     var jackRule = new Jack();
+    //     var revolution = new Revolution();
+    //     var eightRule = new Eight();
+    //     var jokerRule = new Jokers();
+
+    //     var rules = [general, jackRule, revolution, eightRule, jokerRule];
+
+
+    //      // Input: array of 4 Card items with card rank/kind 8 
+        
+    //     var cards = [];
+
+    //     for(var i = 0; i < 4; i++)
+    //     {
+    //         var randSuit = chance.natural({min:1,max:4});
+    //         var newCard = new Card(randSuit,8);
+    //         cards.push(newCard);
+    //     }
+
+    //     var plMove = new PlayerMove(undefined, undefined, cards);
+
+    //     var output = plMove.checkExtraRules(rules);
+
+    //     var nowProperty = output["later"];
+
+    //     expect(nowProperty != undefined).to.equal(true);
+    //     expect(nowProperty.length == 1).to.equal(true);
+    //     expect(nowProperty.pop() == "Revolution").to.equal(true);
+    // });
+
+    it("isMoveEqualTo shall return true when two PlayerMove object is the same",function(){
         
         var cards = [];
+        var randNumOfCards = chance.integer({min:1,max:13});
 
-        for(var i = 0; i < 3; i++)
+        for(var i = 0; i < randNumOfCards; i++)
         {
             var randSuit = chance.natural({min:1,max:4});
-            var newCard = new Card(randSuit, 3);
+            var randKind = chance.natural({min:3,max:15});
+
+            var newCard = new Card(randSuit,randKind);
             cards.push(newCard);
         }
 
-        var plMove = new PlayerMove(undefined, undefined, cards);
+        var plMove1 = new PlayerMove(true, "asdfs", cards);
+        var plMove2 = new PlayerMove(true, "asdfs", cards);
 
-        var output = plMove.checkExtraRules(rules);
+        var isEqual = plMove1.isMoveEqualTo(plMove2);
 
-        expect(output == undefined).to.equal(true);
+        expect(isEqual).to.equal(true);
     });
 
-    it("checkExtraRules shall return an object with the 'now' property that only has 'Jack back', if applicabale rules are jack back without any Eight Enders"
-        ,function(){
-
-        /* initialize rules */
-
-        var general = new GeneralRule();
-        var jackRule = new Jack();
-        var revolution = new Revolution();
-        var eightRule = new Eight();
-        var jokerRule = new Jokers();
-
-        var rules = [general, jackRule, revolution, eightRule, jokerRule];
-
-
-        /* Input: array of 3 Card items with card rank/kind 11 */
+    it("isMoveEqualTo shall return false when two PlayerMove object is the different",function(){
         
-        var cards = [];
 
-        for(var i = 0; i < 3; i++)
+        // Player Move object 1
+
+        var cards = [];
+        var randNumOfCards = chance.integer({min:1,max:13});
+
+        for(var i = 0; i < randNumOfCards; i++)
         {
             var randSuit = chance.natural({min:1,max:4});
-            var newCard = new Card(randSuit,11);
+            var randKind = chance.natural({min:3,max:15});
+
+            var newCard = new Card(randSuit,randKind);
             cards.push(newCard);
         }
 
-        var plMove = new PlayerMove(undefined, undefined, cards);
-
-        var output = plMove.checkExtraRules(rules);
-
-        var nowProperty = output["now"];
-
-        expect(nowProperty != undefined).to.equal(true);
-        expect(nowProperty.length == 1).to.equal(true);
-        expect(nowProperty.pop() == "Jack Back").to.equal(true);
-    });
-
-    it("checkExtraRules shall return an object with the newRound property that has false value, if applicabale rules are jack back without any Eight Enders"
-        ,function(){
-
-        /* initialize rules */
-
-        var general = new GeneralRule();
-        var jackRule = new Jack();
-        var revolution = new Revolution();
-        var eightRule = new Eight();
-        var jokerRule = new Jokers();
-
-        var rules = [general, jackRule, revolution, eightRule, jokerRule];
+        var plMove1 = new PlayerMove(true, "asdfs", cards);
 
 
-        /* Input: array of 3 Card items with card rank/kind 11 */
-        
-        var cards = [];
 
-        for(var i = 0; i < 3; i++)
+        // Player Move object 2
+
+        var cards2 = [];
+
+        for(var i = 0; i < randNumOfCards; i++)
         {
             var randSuit = chance.natural({min:1,max:4});
-            var newCard = new Card(randSuit,11);
-            cards.push(newCard);
+            var randKind = chance.natural({min:3,max:15});
+
+            var newCard = new Card(randSuit,randKind);
+            cards2.push(newCard);
         }
 
-        var plMove = new PlayerMove(undefined, undefined, cards);
+        var plMove2 = new PlayerMove(true, "qwerty", cards2);
 
-        var output = plMove.checkExtraRules(rules);
+        var isEqual = plMove1.isMoveEqualTo(plMove2);
 
-        var nowProperty = output["newRound"];
-
-        expect(nowProperty != undefined).to.equal(true);
-        expect(nowProperty == false).to.equal(true);
+        expect(isEqual).to.equal(false);
     });
 
-    it("checkExtraRules shall return an object with the newRound property that has true value, if applicabale rules are Eight Enders followed by Revolution"
-        ,function(){
 
-        /* initialize rules */
-
-        var general = new GeneralRule();
-        var jackRule = new Jack();
-        var revolution = new Revolution();
-        var eightRule = new Eight();
-        var jokerRule = new Jokers();
-
-        var rules = [general, jackRule, revolution, eightRule, jokerRule];
-
-
-        /* Input: array of 4 Card items with card rank/kind 8 */
-        
-        var cards = [];
-
-        for(var i = 0; i < 4; i++)
-        {
-            var randSuit = chance.natural({min:1,max:4});
-            var newCard = new Card(randSuit,8);
-            cards.push(newCard);
-        }
-
-        var plMove = new PlayerMove(undefined, undefined, cards);
-
-        var output = plMove.checkExtraRules(rules);
-
-        var nowProperty = output["newRound"];
-
-        expect(nowProperty != undefined).to.equal(true);
-        expect(nowProperty == true).to.equal(true);
-    });
-
-    it("checkExtraRules shall return an object with the 'now' property that only has 'Eight Enders', if applicabale rules are Eight Enders followed by Revolution"
-        ,function(){
-
-        /* initialize rules */
-
-        var general = new GeneralRule();
-        var jackRule = new Jack();
-        var revolution = new Revolution();
-        var eightRule = new Eight();
-        var jokerRule = new Jokers();
-
-        var rules = [general, jackRule, revolution, eightRule, jokerRule];
-
-
-         // Input: array of 4 Card items with card rank/kind 8 
-        
-        var cards = [];
-
-        for(var i = 0; i < 4; i++)
-        {
-            var randSuit = chance.natural({min:1,max:4});
-            var newCard = new Card(randSuit,8);
-            cards.push(newCard);
-        }
-
-        var plMove = new PlayerMove(undefined, undefined, cards);
-
-        var output = plMove.checkExtraRules(rules);
-
-        var nowProperty = output["now"];
-
-        expect(nowProperty != undefined).to.equal(true);
-        expect(nowProperty.length == 1).to.equal(true);
-        expect(nowProperty.pop() == "Eight Enders").to.equal(true);
-    });
-
-    it("checkExtraRules shall return an object with the 'later' property that only has 'Revolution', if applicabale rules are Eight Enders followed by Revolution"
-        ,function(){
-
-        /* initialize rules */
-
-        var general = new GeneralRule();
-        var jackRule = new Jack();
-        var revolution = new Revolution();
-        var eightRule = new Eight();
-        var jokerRule = new Jokers();
-
-        var rules = [general, jackRule, revolution, eightRule, jokerRule];
-
-
-         // Input: array of 4 Card items with card rank/kind 8 
-        
-        var cards = [];
-
-        for(var i = 0; i < 4; i++)
-        {
-            var randSuit = chance.natural({min:1,max:4});
-            var newCard = new Card(randSuit,8);
-            cards.push(newCard);
-        }
-
-        var plMove = new PlayerMove(undefined, undefined, cards);
-
-        var output = plMove.checkExtraRules(rules);
-
-        var nowProperty = output["later"];
-
-        expect(nowProperty != undefined).to.equal(true);
-        expect(nowProperty.length == 1).to.equal(true);
-        expect(nowProperty.pop() == "Revolution").to.equal(true);
-    });
 
 
 
