df26ed4e5e6ceae37a9fa108fb335ffbbcfdd20a
adrian hartanto <adrianasdfs@gmail.com>
2017-02-22 14:12:08 +0700

    remove setInterval

--- MessageQueue.js
+++ MessageQueue.js
@@ -4,20 +4,11 @@ function MessageQueue(){}
 
 MessageQueue.send = function send(conn,dt){
 
-    var d = dt;
-    
-    var interval = setInterval(function(){
+    var data = dt.shift();
+    var output = jsonmaker.makeResponseJSON(data.getData(),data.getCode());
 
-        var data = d.shift();
-        var output = jsonmaker.makeResponseJSON(data.getData(),data.getCode());
-
-        conn.send(JSON.stringify(output));        
-
-        if(d.length == 0){
-            clearInterval(interval);
-        }
-
-    },1000);
+    if(conn.readyState == 1)
+    	conn.send(JSON.stringify(output));   
 
 }
 
